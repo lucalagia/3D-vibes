632b932c59b244317f6ebb5cc37f16082fa05126
lucalagia <luca.alagia1@gmail.com>
2024-09-12 18:53:04 +0200

    prototipo_v1.0
    finite le animazione degli zombi con aggro
    finite le meccaniche della pistola

--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -5,8 +5,10 @@ var player = null
 var state_machine
 var health = 6
 
-const SPEED = 1
+const WALKING_SPEED = 0.5
+const RUNNING_SPEED = 4
 const ATTACK_RANGE = 1.5
+const AGGRO_RANGE = 10
 
 @export var player_path := "/root/World/Map/SubViewportContainer/SubViewport/Player"
 
@@ -25,16 +27,27 @@ func _process(delta):
 			# Navigation
 			nav_agent.set_target_position(player.global_transform.origin)
 			var next_nav_point = nav_agent.get_next_path_position()
-			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			velocity = (next_nav_point - global_transform.origin).normalized() * WALKING_SPEED
+			rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), delta * 10)
+		"zombie_running":
+			# Navigation
+			nav_agent.set_target_position(player.global_transform.origin)
+			var next_nav_point = nav_agent.get_next_path_position()
+			velocity = (next_nav_point - global_transform.origin).normalized() * RUNNING_SPEED
 			rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), delta * 10)
 		"Attack":
 			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 
 	# Conditions
+	anim_tree.set("parameters/conditions/aggro", _target_aggro())
 	anim_tree.set("parameters/conditions/Attack", _target_in_range(0.0))
-	anim_tree.set("parameters/conditions/Walking", !_target_in_range(0.0))
+	anim_tree.set("parameters/conditions/Walking", !_target_in_range(0.0) and !_target_aggro())
+	anim_tree.set("parameters/conditions/zombie_running", !_target_in_range(0.0) and _target_aggro())
 	
 	move_and_slide()
+
+func _target_aggro():
+	return global_position.distance_to(player.global_position) < AGGRO_RANGE
 
 func _target_in_range(margin):
 	return global_position.distance_to(player.global_position) < ATTACK_RANGE + margin
@@ -45,9 +58,11 @@ func _hit_finished():
 		player.hit(dir)
 
 
+var original_color : Color
 func _on_area_3d_body_part_hit(dam: Variant) -> void:
 	health -= dam
+	
 	if health <= 0:
 		anim_tree.set("parameters/conditions/die", true)
-		await get_tree().create_timer(4.0).timeout
+		await get_tree().create_timer(5.0).timeout
 		queue_free()
